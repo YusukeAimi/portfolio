bde6d159c5a0040cec3d8153bf4c09bc48806447
Yusuke Aimi <70191847+YusukeAimi@users.noreply.github.com>
2020-10-12 23:42:09 +0900

    Add files via upload

--- check.php
+++ check.php
@@ -28,33 +28,19 @@ if (!empty($_POST)) {
 	header('Location: thanks.php');
 	exit();
 	*/
-	require 'vendor/autoload.php';
-	$dotenv = new DotenvDotenv(__DIR__);
-	$dotenv->load();
- 
-	$api_key           = new SendGrid(getenv('SENDGRID_API_KEY'));
-	$from              = $_ENV['FROM'];
-	$tos               = explode(',', $_ENV['TOS']);
- 
-	$sendgrid = new SendGrid($api_key, array("turn_off_ssl_verification" => true));
-	$email    = new SendGridEmail();
-	$email->setSmtpapiTos($tos)->
-       setFrom($from)->
-       setFromName("送信者名")->
-       setSubject("[sendgrid-php-example] フクロウのお名前は%fullname%さん")->
-       setText("%familyname% さんは何をしていますか？rn 彼は%place%にいます。")->
-       setHtml("<strong> %familyname% さんは何をしていますか？</strong><br />彼は%place%にいます。")->
-       addSubstitution("%fullname%", array("田中 太郎", "佐藤 次郎", "鈴木 三郎"))->
-       addSubstitution("%familyname%", array("田中", "佐藤", "鈴木"))->
-       addSubstitution("%place%", array("%office%", "%home%", "%office%"))->
-       addSection('%office%', '中野')->
-       addSection('%home%', '目黒')->
-       addCategory('category1')->
-       addHeader('X-Sent-Using', 'SendGrid-API')->
-       addAttachment('./gif.gif', 'owl.gif');
- 
-	$response = $sendgrid->send($email);
-	var_dump($response);
+	require __DIR__ . '/../vendor/autoload.php'; // path to vendor/
+
+	$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
+	$email = new SendGrid\Email();
+	$email->addTo('to@exmaple.com')->
+    		setFrom('from@example.com')->
+    		setSubject('件名')->
+    		setText('こんにちは！');
+
+	$sendgrid->send($email);
+	header('Location: thanks.php');
+	exit();
+	
 }
 
 ?>
