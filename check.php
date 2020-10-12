a931fe95411d6f5724d03c854503d215e360ef91
Yusuke Aimi <70191847+YusukeAimi@users.noreply.github.com>
2020-10-12 22:31:21 +0900

    Add files via upload

--- check.php
+++ check.php
@@ -46,7 +46,7 @@ if (!empty($_POST)) {
   	else {
     	echo "メール送信失敗です";
 	}*/
-
+	/*
 	require 'vendor/autoload.php';
 	$email = new \SendGrid\Mail\Mail();
 	$email->setFrom("ekerr310@icloud.com", "送信者A");
@@ -63,9 +63,23 @@ if (!empty($_POST)) {
 		exit();
 	} catch (Exception $e) {
 		echo 'Caught exception: '. $e->getMessage() ."n";
-		header('Location: contact.php');
-		exit();
 	}
+	*/
+	require 'vendor/autoload.php';
+
+	$from = new SendGrid\Email(null, "test@example.com");
+	$subject = "Hello World from the SendGrid PHP Library!";
+	$to = new SendGrid\Email(null, "ekerr310@icloud.com");
+	$content = new SendGrid\Content("text/plain", "Hello, Email!");
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+	$apiKey = getenv('SENDGRID_API_KEY');
+	$sg = new \SendGrid($apiKey);
+
+	$response = $sg->client->mail()->send()->post($mail);
+	echo $response->statusCode();
+	echo $response->headers();
+	echo $response->body();
 }
 
 ?>
