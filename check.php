0c9c5d81bf5480cacb50f0f3ae057e2461c9aee5
Yusuke Aimi <70191847+YusukeAimi@users.noreply.github.com>
2020-10-12 23:03:26 +0900

    Add files via upload

--- check.php
+++ check.php
@@ -66,14 +66,13 @@ if (!empty($_POST)) {
 
 	require 'vendor/autoload.php';
 
-	$email = new \SendGrid\Mail\Mail(); 
-	$email->setFrom("ekerr310@icloud.com", "Example User");
+	$email = new SendGridMailMail(); 
+	$email->setFrom("test@example.com", "Example User");
 	$email->setSubject("Sending with SendGrid is Fun");
 	$email->addTo("ekerr310@icloud.com", "Example User");
 	$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-	$email->addContent(
-    	"text/html", "<strong>and easy to do anywhere, even with PHP</strong>");
-	$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+	$email->addContent("text/html", "<strong>and easy to do anywhere, even with PHP</strong>");
+	$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
 	try {
     	$response = $sendgrid->send($email);
     	print $response->statusCode() . "\n";
@@ -82,6 +81,8 @@ if (!empty($_POST)) {
 	} catch (Exception $e) {
     	echo 'Caught exception: '. $e->getMessage() ."\n";
 	}
+	header('Location: thanks.php');
+	exit();
 }
 
 ?>
